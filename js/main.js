0eabd4dcf6ccb61eb45343dff0c5fbdca970ba2b
noyon-podder <noyon.vai369@gmail.com>
2023-06-27 03:17:24 +0600

    slick slider add this commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-
+$(document).ready(function () {
+  $(".slick-slider-center").slick({
+    centerMode: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "10px",
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
